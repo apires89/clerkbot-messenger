0428a7a47cb71f1959aa8abacd0e9eea5b0c7e9f
andre pires <andrecabralpires@gmail.com>
2017-02-27 15:51:02 +0000

    controllers, still missing hostel controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+resources :rooms
+resources :activities
+resources :bookings
+resources :hostel
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
