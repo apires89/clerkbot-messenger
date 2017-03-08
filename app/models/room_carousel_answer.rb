d9c6822c2eb2d348f43d8ee3c5de35da90bd2543
Guilherme Andrade <guilherme.andrade.ao@gmail.com>
2017-03-08 13:51:32 +0000

    tags

--- app/models/room_carousel_answer.rb
+++ app/models/room_carousel_answer.rb
@@ -17,7 +17,7 @@ class RoomCarouselAnswer < Answer
           {
             title: room.name,
             image_url: "http://res.cloudinary.com/dqltnr3rd/image/upload/" + room.photo.path,
-            subtitle: "Room for " + room.capacity.to_s + " people.\n#{room.description}.",
+            subtitle: "Average price for #{room.capacity} people: #{(user.booking.checkout - user.booking.checkin).to_i*room.price}€\n#{room.description}.",
             buttons:[
               {
                 type: "web_url",
@@ -43,4 +43,5 @@ class RoomCarouselAnswer < Answer
   def change_defaults
     self.quik_replies = false
   end
+
 end
